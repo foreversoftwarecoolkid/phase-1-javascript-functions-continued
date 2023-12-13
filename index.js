db8c95e8f442566bd30e5bca66a8393d688aa5d9
Henry <henry.bogita@student.moringaschool.com>
2023-12-13 14:45:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Example usage:
+  console.log(saturdayFun()); // This Saturday, I want to roller-skate!
+  console.log(saturdayFun("bathe my dog")); // This Saturday, I want to bathe my dog!
+  
+  console.log(mondayWork()); // This Monday, I will go to the office.
+  console.log(mondayWork("work from home")); // This Monday, I will work from home.
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // You are !!!a dedicated programmer!!!"
+  
